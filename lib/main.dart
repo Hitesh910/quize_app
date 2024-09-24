1b2b448dadd21e3aaca9847f32a8ff074652b70e
Hitesh <Hiteshkhatri910@gmail.com>
2024-09-24 18:05:07 +0530

    QuizeApp

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:quize_app/utils/app_routes.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      routes: app_routes,
+    )
+  );
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quize_app/screen/home/view/home_screen.dart';
+
+Map<String,WidgetBuilder> app_routes = {
+  "/":(context) => HomeScreen(),
+};
